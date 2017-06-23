a88b9190ad7e2e21fbe18b09673eebb3352a6e5f
Andrew Bales <agbales@gmail.com>
2017-06-23 02:57:48 -0700

    port update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
+const port = process.env.PORT || 8080;
+var server = app.listen(port);
 
 app.use(express.static('public'));
 
